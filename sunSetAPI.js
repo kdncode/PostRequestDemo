8582751ad8617e06aa64823339945badbc0c38eb
kdncode <kdncode@gmail.com>
2018-04-22 14:39:54 -0500

    added condition in SD, Cali

--- sunSetAPI.js
+++ sunSetAPI.js
@@ -1,8 +1,21 @@
+// Sunset in Hawaii
+
 var request = require("request");
 
 request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22maui%2C%20hi%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys', (err, res, body) => {
     if(!err && res.statusCode == 200){
         var parsedData = JSON.parse(body)
         console.log(parsedData["query"]["results"]["channel"]["astronomy"]["sunset"])
+    }
+})
+
+//Current conditions for San Diego CA
+
+var request = require("request");
+
+request("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", (err, res, body ) => {
+    if (!err && res.statusCode == 200 ) {
+        var data = JSON.parse(body)
+        console.log(data["query"]["results"]["channel"]["item"]["condition"]["temp"]);
     }
 })
