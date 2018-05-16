8e9f3c99d4c5c8bbe6a1b98bf51990ba2b15ef69
kdncode <kdncode@gmail.com>
2018-05-15 23:34:42 -0500

    updated

--- sunSetAPI.js
+++ sunSetAPI.js
@@ -9,7 +9,6 @@ request('https://query.yahooapis.com/v1/public/yql?q=select%20astronomy.sunset%2
 })
 
 //Current conditions for San Diego CA
-
 var request = require("request");
 
 request("https://query.yahooapis.com/v1/public/yql?q=select%20item.condition%20from%20weather.forecast%20where%20woeid%20%3D%202487889&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys", (err, res, body ) => {
